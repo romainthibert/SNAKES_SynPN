010870031819dfb896e7393b6f2826b488554233
Romain Thibert <romain.thibert@ens-paris-saclay.fr>
2022-07-08 15:15:58 +0000

    add simulation method to class PetriNet to simulate a net giving input events

--- synpn.py
+++ synpn.py
@@ -10,6 +10,35 @@ def extend(module):
             self.activeEvent = None
             module.PetriNet.__init__(self,name)
 
+        def simulation(self):
+            print("Simulation start. Current activeEvent is {}".format(self.activeEvent))
+            run=True
+            while run:
+                stable=False
+                while not stable:
+                    stable=True
+                    for t in self.transition():
+                        bind=t.modes()
+                        if t.enabled(bind):
+                            t.fire(bind)
+                            print(t,"fired")
+                            stable=False
+
+                nextEvent=input("Stable marking reached.\n====\nNext event ? (q to exit)\n")
+
+                if nextEvent == 'q':
+                    break
+
+                self.activeEvent=nextEvent
+                for t in self.transition():
+                    bind=t.modes()
+                    if t.enabled(bind):
+                        t.fire(bind)
+                        print(t,"fired")
+                self.activeEvent=None
+                print("Event reset\n====")
+
+
     class Transition(module.Transition):
         def __init__(self, name, PN, guard=None, **args):
             self.inputLabel = args.pop("inputLabel",None)
@@ -19,7 +48,7 @@ def extend(module):
         def enabled(self, binding):
             if self.inputLabel == self.PN.activeEvent:
                 return module.Transition.enabled(self, binding)
-            elif self.inputLabel == None:
+            elif self.inputLabel == None and self.PN.activeEvent == None:
                 return module.Transition.enabled(self, binding)
             else:
                 return False
